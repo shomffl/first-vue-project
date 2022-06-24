63608fd76e1400b62e8f0acfbb4103e78c1b192c
sho watanabe <showatanabe0226@gmail.com>
2022-06-24 23:27:06 +0900

    [add]リストとインプットの部分をコンポーネント化

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import TweetPostForm from "./TweetPostForm.vue";
+import TweetList from "./TweetList.vue";
 const tweets = ref([
   { id: 0, description: "Hello, world!" },
   { id: 1, description: "Hello, world!" },
@@ -19,19 +21,11 @@ const deleteTweet = (id: number) => {
 <template>
   <div class="container">
     <h1>Tweeter</h1>
-    <div class="form-container">
-      <input v-model="inputtingDescription" />
-      <button class="save-button" @click="postTweet()">post</button>
-    </div>
+    <TweetPostForm />
     <div class="tweet-container">
       <p v-if="tweets.length <= 0">No tweets have been added</p>
       <ul>
-        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
-          <span>{{ tweet.description }}</span>
-          <button class="delete-button" @click="deleteTweet(tweet.id)">
-            delete
-          </button>
-        </li>
+        <TweetList :tweets="tweets" />
       </ul>
     </div>
   </div>
@@ -42,56 +36,5 @@ const deleteTweet = (id: number) => {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
-
-.form-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: aliceblue;
-  padding: 24px 0;
-  width: 60%;
-  margin-bottom: 12px;
-  border-radius: 4px;
-}
-
-.tweet-list {
-  list-style: none;
-  margin-bottom: 12px;
-  border-radius: 4px;
-  font-size: 12px;
-  display: flex;
-  justify-content: space-between;
-  background-color: rgb(188, 216, 239);
-  padding: 8px 20px;
-  width: 300px;
-}
-
-.save-button {
-  color: #fff;
-  font-weight: bold;
-  background-color: #68c9c9;
-  border-radius: 2px;
-  border: none;
-  width: 60px;
-  height: 22px;
-}
-
-.save-button:hover {
-  background-color: #67f4f4;
-  transition: 0.5s;
-}
-.delete-button {
-  color: #fff;
-  font-weight: bold;
-  background-color: #c96868;
-  border-radius: 2px;
-  border: none;
-  width: 60px;
-  height: 22px;
-}
-.delete-button:hover {
-  background-color: #f49167;
-  transition: 0.5s;
 }
 </style>
---
+++ src/components/TweetList.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+type Tweet = {
+  id: number;
+  description: string;
+};
+
+type Props = {
+  tweets: Tweet[];
+};
+
+const deleteTweet = (id: number) => {};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+    <span>{{ tweet.description }}</span>
+    <button class="delete-button" @click="deleteTweet(tweet.id)">delete</button>
+  </li>
+</template>
+
+<style scoped>
+.tweet-list {
+  list-style: none;
+  margin-bottom: 12px;
+  border-radius: 4px;
+  font-size: 12px;
+  display: flex;
+  justify-content: space-between;
+  background-color: rgb(188, 216, 239);
+  padding: 8px 20px;
+  width: 300px;
+}
+.delete-button {
+  color: #fff;
+  font-weight: bold;
+  background-color: #c96868;
+  border-radius: 2px;
+  border: none;
+  width: 60px;
+  height: 22px;
+}
+.delete-button:hover {
+  background-color: #f49167;
+  transition: 0.5s;
+}
+</style>
